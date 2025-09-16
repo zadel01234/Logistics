3a3f66625212b938d817546c07f66117c74ddbf1
Olufayo Emmanuel <olufalzy01@gmail.com>
2025-09-16 22:11:32 +0100

    github pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+
+// export default defineConfig({
+//   base: "/Logistics/",
+//   plugins: [react()],
+// });
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Logistics/",
   plugins: [react()],
-});
+  base: "/Logistics/", // 👈 important
+})
